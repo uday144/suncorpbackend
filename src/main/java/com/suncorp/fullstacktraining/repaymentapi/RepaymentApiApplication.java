d23557ef510adc718fd1ab246fbd2a3e96ae06de
Uday <uday144@gmail.com>
2018-05-19 11:58:41 +0530

    First commit

---
+++ src/main/java/com/suncorp/fullstacktraining/repaymentapi/RepaymentApiApplication.java
@@ -0,0 +1,12 @@
+package com.suncorp.fullstacktraining.repaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepaymentApiApplication.class, args);
+	}
+}
